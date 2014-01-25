453189aafaca76fa2a83da40d331e6559c0bd3ae
Michael Helmling <michaelhelmling@posteo.de>
2014-01-25 20:12:44 +0100

    Fix behavior of noidle in the presence of pending changes
    According to the protocol specification, the output of the "noidle"
    command is in principal the same as that of "idle", but might be empty.
    Without this patch, python-mpd2 raises a ProtocolError if the response
    to noidle is nonempty.

--- mpd.py
+++ mpd.py
@@ -417,12 +417,7 @@ class MPDClient(object):
           raise CommandError('cannot send noidle if send_idle was not called')
         del self._pending[0]
         self._write_command("noidle")
-        status = self._read_list()
-        # In some case mpd has already write the changed line
-        if status is not None:
-            # So we need to fetch the result of the noidle
-            self._fetch_nothing()
-        return None
+        return self._fetch_list()
 
     def _hello(self):
         line = self._rfile.readline()
