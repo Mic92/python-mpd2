fc040afe635ddf6c92c40f86bbb5f5a1c7f7350e
Robert Niederreiter <office@squarewave.at>
2016-09-06 10:17:30 +0200

    Add twisted example

---
+++ examples/twisted.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+from mpd import MPDProtocol
+from twisted.internet import protocol
+from twisted.internet import reactor
+
+
+class MPDApp(object):
+    # Example application which deals with MPD
+
+    def __init__(self, protocol):
+        self.protocol = protocol
+
+    def __call__(self, result):
+        # idle result callback
+        print(result)
+
+        def status_success(result):
+            # status query success
+            print(result)
+
+        def status_error(result):
+            # status query failure
+            print(result)
+
+        # query player status
+        self.protocol.status()\
+            .addCallback(status_success)\
+            .addErrback(status_error)
+
+
+class MPDClientFactory(protocol.ClientFactory):
+    protocol = MPDProtocol
+
+    def buildProtocol(self, addr):
+        print("Create MPD protocol")
+        protocol = self.protocol()
+        protocol.factory = self
+        protocol.idle_result = MPDApp(protocol)
+        return protocol
+
+    def clientConnectionFailed(self, connector, reason):
+        print("Connection failed - goodbye!")
+        reactor.stop()
+    
+    def clientConnectionLost(self, connector, reason):
+        print("Connection lost - goodbye!")
+        reactor.stop()
+
+
+if __name__ == '__main__':
+    factory = MPDClientFactory()
+    reactor.connectTCP("localhost", 6600, factory)
+    reactor.run()
