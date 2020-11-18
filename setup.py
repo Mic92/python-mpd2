57673145521e6cc9bafdafd090cc92ab911f997c
Jörg Thalheim <joerg@thalheim.io>
2020-11-18 10:30:56 +0100

    set python3.6 as minimum

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ def read(fname):
 setup(
     name="python-mpd2",
     version=VERSION,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
     description="A Python MPD client library",
     long_description=read('README.rst'),
     classifiers=CLASSIFIERS,
