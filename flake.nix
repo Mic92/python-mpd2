6d956de609277b88c1ce93e9ac37c2b9a4ffb6bb
Jörg Thalheim <joerg@thalheim.io>
2023-02-18 06:07:28 +0100

    switch to flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development environment for this project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ lib, ... }: {
+      systems = lib.systems.flakeExposed;
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bashInteractive
+            python38
+            python39
+            (python310.withPackages(ps: [ps.setuptools ps.tox ps.wheel]))
+            python311
+            python312
+            pypy3
+            twine
+          ];
+          shellHook = ''
+            # breaks python36/python37
+            unset _PYTHON_SYSCONFIGDATA_NAME
+          '';
+        };
+      };
+    });
+}
