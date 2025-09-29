6f6e5432694622a2e4c9cb831cff15d1e5c988f9
Jörg Thalheim <joerg@thalheim.io>
2025-09-29 14:59:09 +0200

    only build on limited platforms

--- flake.nix
+++ flake.nix
@@ -9,42 +9,55 @@
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ flake-parts, treefmt-nix, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } ({ lib, ... }: {
+  outputs =
+    inputs@{ flake-parts, treefmt-nix, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         treefmt-nix.flakeModule
       ];
-      systems = lib.systems.flakeExposed;
-      perSystem = { pkgs, config, ... }: {
-        treefmt = {
-          projectRootFile = "flake.nix";
-          programs = {
-            ruff.format = true;
-            ruff.check = true;
-            mypy.enable = true;
-            mypy.directories = {
-              "." = {
-                modules = [ "mpd" ];
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { pkgs, config, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              ruff.format = true;
+              ruff.check = true;
+              mypy.enable = true;
+              mypy.directories = {
+                "." = {
+                  modules = [ "mpd" ];
+                };
               };
             };
           };
-        };
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            bashInteractive
-            python310
-            python311
-            python312
-            (python313.withPackages(ps: [ps.setuptools ps.tox ps.wheel ps.build]))
-            python314
-            pypy3
-            twine
-            mypy
-            ruff
-            config.treefmt.build.wrapper
-          ];
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              bashInteractive
+              python310
+              python311
+              python312
+              (python313.withPackages (ps: [
+                ps.setuptools
+                ps.tox
+                ps.wheel
+                ps.build
+              ]))
+              python314
+              pypy3
+              twine
+              mypy
+              ruff
+              config.treefmt.build.wrapper
+            ];
+          };
         };
-      };
-    });
+    };
 }
